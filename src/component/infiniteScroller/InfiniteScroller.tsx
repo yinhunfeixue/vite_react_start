d8fe204258ba84e36e94139fd56df1a91a2ffdd6
徐俊杰 <xujunjie1@xiaomi.com>
2025-11-04 20:20:15 +0800

    滚动加载

---
+++ src/component/infiniteScroller/InfiniteScroller.tsx
@@ -0,0 +1,119 @@
+import { useInfiniteScroll } from 'ahooks';
+import { Data } from 'ahooks/lib/useInfiniteScroll/types';
+import classNames from 'classnames';
+import React, { CSSProperties, useRef } from 'react';
+import './InfiniteScroller.less';
+
+interface IInfiniteScrollerProps<T> {
+  className?: string;
+  style?: CSSProperties;
+
+  /**
+   * 起始页码
+   *
+   * @default 1
+   */
+  startPage?: number;
+
+  /**
+   * 滚动方向
+   *
+   * @default 'bottom'
+   */
+  direction?: 'top' | 'bottom';
+
+  requestFunction: (page: number) => Promise<{ total: number; list: T[] }>;
+
+  render: (
+    data: IInfiniteScrollerData | undefined,
+    func: {
+      mutate: (data: IInfiniteScrollerData) => void;
+      reload: () => void;
+    },
+  ) => React.ReactNode;
+}
+
+interface IInfiniteScrollerData extends Data {
+  page: number;
+  total: number;
+}
+
+/**
+ * InfiniteScroller
+ *
+ * @example
+ * ```tsx
+ * <InfiniteScroller
+ *  style={{ border: '1px solid red', height: 600 }}
+ *  requestFunction={async (page) => {
+ *    return {
+ *      list: [],  // 数据列表
+ *      total: 123,
+ *    };
+ *  }}
+ *  render={(data, { mutate, reload }) => {
+ *    return (
+ *      <div>
+ *        <button onClick={() => reload()}>刷新</button>
+ *        {data?.list.map((item) => (
+ *          <div key={item.id}>{item.name}</div>
+ *        ))}
+ *      </div>
+ *    );
+ *  }}
+ * />
+ * ```
+ */
+function InfiniteScroller<T>(props: IInfiniteScrollerProps<T>) {
+  const {
+    className,
+    style,
+    direction,
+    startPage = 1,
+    requestFunction,
+    render,
+  } = props;
+  const rootRef = useRef(null);
+
+  const { data, loading, loadingMore, mutate, reload } =
+    useInfiniteScroll<IInfiniteScrollerData>(
+      async (currentData) => {
+        const nextPage = currentData ? currentData.page + 1 : startPage;
+        const res = await requestFunction(nextPage);
+        return {
+          ...res,
+          page: nextPage,
+        };
+      },
+      {
+        target: rootRef,
+        direction,
+        threshold: 200,
+        isNoMore: (data) => {
+          return data ? data.list.length >= data.total : false;
+        },
+      },
+    );
+
+  const renderFooter = () => {
+    if (loading || loadingMore) {
+      return <div className='InfiniteScrollerFooter'>加载中...</div>;
+    }
+    if (data && data.list.length >= data.total) {
+      return <div className='InfiniteScrollerFooter'>- 我是有底线的 - </div>;
+    }
+    return null;
+  };
+
+  return (
+    <div
+      className={classNames('InfiniteScroller', className)}
+      style={style}
+      ref={rootRef}
+    >
+      {render(data, { mutate, reload })}
+      {renderFooter()}
+    </div>
+  );
+}
+export default React.memo(InfiniteScroller);
--- src/pages/user/UserListPage.tsx
+++ src/pages/user/UserListPage.tsx
@@ -1,4 +1,6 @@
-import userProjectStore from '@/model/ProjectStore';
+import InfiniteScroller from '@/component/infiniteScroller/InfiniteScroller';
+import ProjectUtil from '@/utils/ProjectUtil';
+import { Button } from 'antd';
 import classNames from 'classnames';
 import styles from './UserListPage.module.less';
 
@@ -6,10 +8,60 @@ import styles from './UserListPage.module.less';
  * UserListPage
  */
 function UserListPage() {
-  const { token } = userProjectStore();
   return (
     <div className={classNames(styles.UserListPage)}>
-      {token ? '已登录' : '未登录'}
+      <InfiniteScroller
+        style={{ border: '1px solid red', height: 600 }}
+        requestFunction={async (page) => {
+          await ProjectUtil.sleep();
+          const res = new Array(10).fill(0).map((_, index) => {
+            return {
+              id: (page - 1) * 10 + index + 1,
+              name: `User ${(page - 1) * 10 + index + 1}`,
+            };
+          });
+          return {
+            list: res,
+            total: 35,
+          };
+        }}
+        render={(data, func) => {
+          const { mutate, reload } = func;
+          return (
+            <div>
+              {data?.list.map((item, index) => {
+                return (
+                  <div
+                    key={item.id}
+                    style={{ padding: 8, borderBottom: '1px solid #eee' }}
+                  >
+                    {item.name}
+                    <Button
+                      onClick={() => {
+                        const newData = { ...data };
+                        newData.list.splice(index, 1);
+                        newData.total -= 1;
+                        mutate?.(newData);
+                      }}
+                    >
+                      删除
+                    </Button>
+                    <Button
+                      onClick={() => {
+                        item.name = Math.random();
+                        mutate?.({ ...data });
+                      }}
+                    >
+                      修改
+                    </Button>
+                  </div>
+                );
+              })}
+              <Button onClick={() => reload()}>刷新</Button>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
