3c3ba63d318f5716f139638c60241e6f8d30f359
徐俊杰 <xujunjie1@xiaomi.com>
2026-01-27 11:06:30 +0800

    更新依赖包

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,10 @@ import js from '@eslint/js';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
-export default tseslint.config(
+export default defineConfig(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
